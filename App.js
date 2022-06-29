40b332c0ba2ac2d196241d255f3d6c3939e6b9c4
Bruno Lara <brunoglara@gmail.com>
2022-06-28 21:13:33 -0300

    Create pages and create navigations

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import Task from './src/pages/Task';
+import NewTask from './src/pages/NewTask';
+import Details from './src/pages/Details';
+
+const Stack = createNativeStackNavigator()
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Task'>
+        <Stack.Screen
+          name='Task'
+          component={Task}
+          options={{ headerTintColor: '#f92e6a' }}
+        />
+        <Stack.Screen
+          name='NewTask'
+          component={NewTask}
+          options={{ headerTintColor: '#f92e6a' }}
+        />
+
+        <Stack.Screen
+          name='Details'
+          component={Details}
+          options={{ headerTintColor: '#f92e6a' }}
+        />
+
+      </Stack.Navigator>
+
+    </NavigationContainer>
   );
 }
 
