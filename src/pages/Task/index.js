52f9bab37517d15a80def056315217bb8f5bfd1a
Bruno Lara <brunoglara@gmail.com>
2022-06-28 22:04:54 -0300

    init configure page Task

--- src/pages/Task/index.js
+++ src/pages/Task/index.js
@@ -7,12 +7,43 @@ import {
     FlatList
 } from 'react-native';
 
+import { FontAwesome } from '@expo/vector-icons'
+
+import { collection, getDocs } from "firebase/firestore";
+import database from '../../config/firebaseConfig';
+
 import styles from "./styles"
 
-export default function Task() {
+
+async function getData() {
+    const querySnapshot = await getDocs(collection(database, "Tasks"));
+
+    const list = []
+    querySnapshot.forEach((doc) => {
+        list.push({ ...doc.data(), id: doc.id })
+    });
+    return list
+}
+
+
+export default function Task({ navigation }) {
+    const [task, SetTask] = useState([])
+
+    useEffect(() => {
+        SetTask(getData())
+    }, [])
+
     return (
-        <SafeAreaView style={styles.container}>
-            <Text>Page Details</Text>
-        </SafeAreaView>
+        <View style={styles.container}>
+            <FlatList
+            />
+
+            <TouchableOpacity
+                style={styles.buttonNewTask}
+                activeOpacity={0.6}
+            >
+                <Text style={styles.iconButton}>+</Text>
+            </TouchableOpacity>
+        </View>
     )
 }
--- src/pages/Task/styles.js
+++ src/pages/Task/styles.js
@@ -4,9 +4,25 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        paddingTop: 20,
     },
+    buttonNewTask: {
+        position: 'absolute',
+        width: 60,
+        height: 60,
+        bottom: 30,
+        left: 20,
+        backgroundColor: '#f92e6a',
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    iconButton: {
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+
 });
 
 export default styles;
